e1cd05534aa23935c9cf134348cad6bba317538d
Mitchell <35324660+Mansalu@users.noreply.github.com>
2020-03-17 14:54:52 -0600

    revert back to ClusterIP service discovery

--- install-cvmfs.sh
+++ install-cvmfs.sh
@@ -32,7 +32,9 @@ echo "Adding CVMFS"
 
 kubectl create namespace cvmfs
 
-yum install git 
+yum -y install git 
+
+export CLUSTER_IP=$(kubectl get --namespace slate-group-$INITIALGROUP -o jsonpath="{.items[0].spec.clusterIP}" services)
 
 git clone https://github.com/Mansalu/prp-osg-cvmfs.git
 
@@ -46,7 +48,7 @@ cat << EOF > default.local
 CVMFS_SERVER_URL="http://cvmfs-s1bnl.opensciencegrid.org:8000/cvmfs/@fqrn@;http://cvmfs-s1fnal.opensciencegrid.org:8000/cvmfs/@fqrn@;http://cvmfs-s1goc.opensciencegrid.org:8000/cvmfs/@fqrn@"
 CVMFS_KEYS_DIR=/etc/cvmfs/keys/opensciencegrid.org/
 CVMFS_USE_GEOAPI=yes
-CVMFS_HTTP_PROXY="http://osg-frontier-squid-cvmfs.slate-group-$INITIALGROUP.svc.local:3128"
+CVMFS_HTTP_PROXY="http://$CLUSTER_IP:3128"
 CVMFS_QUOTA_LIMIT=5000
 CVMFS_REPOSITORIES=atlas.cern.ch,atlas-condb.cern.ch,atlas-nightlies.cern.ch,sft.cern.ch,geant4.cern.ch,grid.cern.ch,cms.cern.ch,oasis.opensciencegrid.org
 EOF
